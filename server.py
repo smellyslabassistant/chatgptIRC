b153ee25ee39586ec13f4949ace813a239ed921f
smellyslabassistant <112456667+smellyslabassistant@users.noreply.github.com>
2023-05-15 09:01:49 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,88 +1,70 @@
 import socket
-import threading
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
+from threading import Thread
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from termcolor import colored
+import time
 
-class IRCServer:
-    def __init__(self):
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind(("127.0.0.1", 8888))
-        self.server_socket.listen(5)
 
-        self.clients = {}
-        self.lock = threading.Lock()
+KEY = b'Sixteen byte key'  # 16-byte key for AES encryption
+PORT = 5001
+BUFFER_SIZE = 1024
 
-    def start(self):
-        print("Server started. Listening for connections...")
+def encrypt(plain_text):
+    cipher = AES.new(KEY, AES.MODE_ECB)
+    cipher_text = cipher.encrypt(pad(plain_text.encode(), AES.block_size))
+    return cipher_text
 
-        while True:
-            client_socket, client_address = self.server_socket.accept()
-            client_thread = threading.Thread(target=self.handle_client, args=(client_socket,))
-            client_thread.start()
+def decrypt(cipher_text):
+    cipher = AES.new(KEY, AES.MODE_ECB)
+    plain_text = unpad(cipher.decrypt(cipher_text), AES.block_size)
+    return plain_text.decode()
 
-    def handle_client(self, client_socket):
-        username = self.receive_message(client_socket)
-        iv = self.receive_message(client_socket)
-        key = self.receive_message(client_socket)
+def client_handler(client_socket, client_address, clients, usernames):
+    username = client_socket.recv(BUFFER_SIZE).decode()
+    print(colored(f"[*] New connection from {client_address[0]}: {username}", 'green'))
+    clients.append(client_socket)
+    usernames[client_socket] = username
 
-        cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
-
-        with self.lock:
-            self.clients[client_socket] = (username, cipher)
-
-        self.send_message(client_socket, "Welcome to the IRC server!\n")
-
-        while True:
-            try:
-                encrypted_message = self.receive_message(client_socket)
-                message = self.decrypt_message(encrypted_message, cipher)
-
-                if message == "/quit":
-                    self.disconnect_client(client_socket)
-                    break
-
-                self.broadcast_message(message, client_socket)
-
-            except ConnectionResetError:
-                self.disconnect_client(client_socket)
+    while True:
+        try:
+            cipher_text = client_socket.recv(BUFFER_SIZE)
+            message = decrypt(cipher_text)
+            if message == "/quit":
+                client_socket.send(encrypt("quitting."))
+                time.sleep(.200)
+                client_socket.send(encrypt("quitting.."))
+                time.sleep(.200)
+                client_socket.send(encrypt("quitting..."))
+                client_socket.close()
+                clients.remove(client_socket)
+                del usernames[client_socket]
+                print(colored(f"[-] Disconnected: {username}", 'red'))
                 break
+            else:
+                print(colored(f"{username}: {message}", 'blue'))
+                for client in clients:
+                    if client != client_socket:
+                        client.send(encrypt(f"{username}: {message}"))
+        except:
+            client_socket.close()
+            clients.remove(client_socket)
+            del usernames[client_socket]
+            print(colored(f"[-] Disconnected: {username}", 'red'))
+            break
 
-    def disconnect_client(self, client_socket):
-        with self.lock:
-            username, _ = self.clients[client_socket]
-            del self.clients[client_socket]
-        client_socket.close()
-        self.broadcast_message(f"{username} has left the chat.\n", client_socket)
+def start_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('localhost', PORT))
+    server_socket.listen(5)
+    print(colored("[+] Server started!", 'green'))
 
-    def broadcast_message(self, message, sender_socket):
-        with self.lock:
-            sender_username, _ = self.clients[sender_socket]
+    clients = []
+    usernames = {}
+    while True:
+        client_socket, client_address = server_socket.accept()
+        thread = Thread(target=client_handler, args=(client_socket, client_address, clients, usernames))
+        thread.start()
 
-            for client, (username, cipher) in self.clients.items():
-                if client != sender_socket:
-                    encrypted_message = self.encrypt_message(f"{sender_username}: {message}", cipher)
-                    self.send_message(client, encrypted_message)
-
-    def send_message(self, client_socket, message):
-        client_socket.send(message.encode())
-
-    def receive_message(self, client_socket):
-        return client_socket.recv(1024).decode().strip()
-
-    def encrypt_message(self, message, cipher):
-        padder = padding.PKCS7(128).padder()
-        padded_data = padder.update(message.encode()) + padder.finalize()
-        encryptor = cipher.encryptor()
-        encrypted_message = encryptor.update(padded_data) + encryptor.finalize()
-        return encrypted_message
-
-    def decrypt_message(self, encrypted_message, cipher):
-        decryptor = cipher.decryptor()
-        padded_data = decryptor.update(encrypted_message) + decryptor.finalize()
-        unpadder = padding.PKCS7(128).unpadder()
-        message = unpadder.update(padded_data) + unpadder.finalize()
-        return message.decode()
-
-# Start the IRC server
-server = IRCServer()
-server.start()
+if __name__ == '__main__':
+    start_server()
