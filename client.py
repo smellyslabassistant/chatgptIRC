ec34d4f41c95c0f220b5fb1b73ebe88e7598dade
smellyslabassistant <112456667+smellyslabassistant@users.noreply.github.com>
2023-05-15 09:02:10 -0500

    Update client.py

--- client.py
+++ client.py
@@ -1,67 +1,56 @@
 import socket
-import threading
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
+from threading import Thread
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from termcolor import colored
+import time
 
-class IRCClient:
-    def __init__(self):
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect(("127.0.0.1", 8888))
+KEY = b'Sixteen byte key'  # 16-byte key for AES encryption
+PORT = 5001
+BUFFER_SIZE = 1024
 
-        self.username = input("Enter your username: ")
-        self.iv = b'0000000000000000'  # Initialization vector (IV)
-        self.key = b'0123456789abcdef'  # Encryption key
+def encrypt(plain_text):
+    cipher = AES.new(KEY, AES.MODE_ECB)
+    cipher_text = cipher.encrypt(pad(plain_text.encode(), AES.block_size))
+    return cipher_text
 
-        self.cipher = Cipher(algorithms.AES(self.key), modes.CBC(self.iv), backend=default_backend())
+def decrypt(cipher_text):
+    cipher = AES.new(KEY, AES.MODE_ECB)
+    plain_text = unpad(cipher.decrypt(cipher_text), AES.block_size)
+    return plain_text.decode()
 
-        self.send_message(self.username)
-        self.send_message(self.iv)
-        self.send_message(self.key)
+def receive_messages(client_socket):
+    while True:
+        cipher_text = client_socket.recv(BUFFER_SIZE)
+        message = decrypt(cipher_text)
+        if message == "quit":
+            client_socket.close()
+            break
+        else:
+            print(colored(message, 'blue'))
 
-        self.receive_thread = threading.Thread(target=self.receive_messages)
-        self.receive_thread.start()
+def send_messages(client_socket, username):
+    while True:
+        message = input()
+        cipher_text = encrypt(message)
+        client_socket.send(cipher_text)
+        if message == "/quit":
+            time.sleep(.600)
+            client_socket.close()
+            break
 
-    def receive_messages(self):
-        while True:
-            try:
-                encrypted_message = self.receive_message()
-                message = self.decrypt_message(encrypted_message, self.cipher)
-                print(message)
+def start_client():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', PORT))
 
-            except ConnectionResetError:
-                print("Disconnected from the server.")
-                self.client_socket.close()
-                break
+    username = input("Enter your username: ")
+    client_socket.send(username.encode())
 
-    def send_message(self, message):
-        encrypted_message = self.encrypt_message(message, self.cipher)
-        self.client_socket.send(encrypted_message)
+    receive_thread = Thread(target=receive_messages, args=(client_socket,))
+    send_thread = Thread(target=send_messages, args=(client_socket, username))
 
-    def receive_message(self):
-        return self.client_socket.recv(1024)
+    receive_thread.start()
+    send_thread.start()
 
-    def encrypt_message(self, message, cipher):
-        padder = padding.PKCS7(128).padder()
-        padded_data = padder.update(message.encode()) + padder.finalize()
-        encryptor = cipher.encryptor()
-        encrypted_message = encryptor.update(padded_data) + encryptor.finalize()
-        return encrypted_message
-
-    def decrypt_message(self, encrypted_message, cipher):
-        decryptor = cipher.decryptor()
-        padded_data = decryptor.update(encrypted_message) + decryptor.finalize()
-        unpadder = padding.PKCS7(128).unpadder()
-        message = unpadder.update(padded_data) + unpadder.finalize()
-        return message.decode()
-
-    def run(self):
-        while True:
-            message = input()
-            if message == "/quit":
-                self.client_socket.close()
-                break
-            self.send_message(message)
-
-# Start the IRC client
-client = IRCClient()
-client.run()
+if __name__ == '__main__':
+    start_client()
